b54363ea158a8ccc481fbe023a84f65993f88d21
Richard Cho <richardcho100@gmail.com>
2020-05-08 18:36:06 -0400

    Combine reducers & create store w/ thunk & redux devtools

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import users from './reducers/users'
+import thunk from 'redux-thunk';
+
+
+const reducer = combineReducers({
+    users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store

5ecd90a3d6d401d602dc62816b7c78aa9677fa90
Richard Cho <richardcho100@gmail.com>
2020-05-14 19:08:37 -0400

    Connect to store & handle change in input

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { updateLoginForm } from '../actions/loginForm'
 
-const Login = () => {
+
+const Login = ({ loginFormData, updateLoginForm }) => {
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const newLoginInfo = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(newLoginInfo)
+    }
+
     return (
        <div>
         <h1>Login</h1>
         <form>
             <label htmlFor='email'>Email:</label>
-            <input type='text' placeholder='Email' name='email' />
+            <input type='text' placeholder='Email' name='email' value={loginFormData.email} onChange={handleInputChange} />
             <label htmlFor='password'>Password:</label>
-            <input type='text' placeholder='Password' name='password' />
+            <input type='text' placeholder='Password' name='password' value={loginFormData.password} onChange={handleInputChange} />
             <input type='submit' value='Log In' />
         </form>
        </div>
     )
 }
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        loginFormData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updateLoginForm })(Login)

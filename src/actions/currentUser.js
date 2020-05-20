7b2ed7935362eb59cc686064989c522d86f1e73f
Richard Cho <richardcho100@gmail.com>
2020-05-20 17:08:13 -0400

    Add login action creator & use to send credentials when submitted via form

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -4,3 +4,25 @@ export const setCurrentUser = user => {
         user
     }
 }
+
+export const login = credentials => {
+    console.log('credentials are', credentials)
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/login", {
+            method: 'Post',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(resp => resp.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(error => console.log(error))
+    }
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from '../actions/loginForm'
+import { login } from '../actions/currentUser'
 
-
-const Login = ({ loginFormData, updateLoginForm }) => {
+const Login = ({ loginFormData, updateLoginForm, login }) => {
     const handleInputChange = event => {
         const { name, value } = event.target
         const newLoginInfo = {
@@ -13,10 +13,15 @@ const Login = ({ loginFormData, updateLoginForm }) => {
         updateLoginForm(newLoginInfo)
     }
 
+    const handleSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
     return (
        <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor='email'>Email:</label>
             <input type='text' placeholder='Email' name='email' value={loginFormData.email} onChange={handleInputChange} />
             <label htmlFor='password'>Password:</label>
@@ -33,4 +38,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { updateLoginForm })(Login)
+export default connect(mapStateToProps, { updateLoginForm, login })(Login)

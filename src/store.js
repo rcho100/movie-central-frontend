7a05334aa90669057812c8f5f54a3ded5f90352c
Richard Cho <richardcho100@gmail.com>
2020-05-26 17:05:21 -0400

    Add reducer signupForm w/update case & pass into root reducer

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import users from './reducers/users'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
+import signupForm from './reducers/signupForm'
 import thunk from 'redux-thunk';
 
 
 const reducer = combineReducers({
     users,
     currentUser,
-    loginForm
+    loginForm,
+    signupForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;

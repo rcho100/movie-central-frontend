1b07fef178c23d534934caacc8a57469835567f7
Richard Cho <richardcho100@gmail.com>
2020-05-27 23:15:44 -0400

    Add submit functionality to form to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { updateSignupForm } from '../actions/signupForm'
+import { signup } from '../actions/currentUser'
 
 
-const Signup = ({ signupFormData, updateSignupForm }) => {
+const Signup = ({ signupFormData, updateSignupForm, signup }) => {
+
     const handleInputChange = event => {
         const { name, value } = event.target
         const newSignupInfo = {
@@ -13,16 +15,22 @@ const Signup = ({ signupFormData, updateSignupForm }) => {
         updateSignupForm(newSignupInfo)
     }
 
+    const handleSubmit = event => {
+        event.preventDefault()
+        signup(signupFormData)
+    }
+
     return (
         <div>
             <h1>Sign Up</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor='name'>Username:</label>
                 <input type='text' placeholder='Username' name='name' value={signupFormData.name} onChange={handleInputChange} />
                 <label htmlFor='email'>Email:</label>
                 <input type='text' placeholder='Email' name='email' value={signupFormData.email} onChange={handleInputChange} />
                 <label htmlFor='password'>Password:</label>
                 <input type='text' placeholder='Password' name='password' value={signupFormData.password} onChange={handleInputChange} />
+                <input type='submit' value='Sign Up' />
             </form>
         </div>
     )
@@ -34,4 +42,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { updateSignupForm })(Signup)
+export default connect(mapStateToProps, { updateSignupForm, signup })(Signup)

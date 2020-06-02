f14b4a448d6f9874de57619a268a94f117b9e32b
Richard Cho <richardcho100@gmail.com>
2020-06-02 11:32:19 -0400

    Create Layout & wrap around router in App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Logout from './components/Logout'
+import Layout from './components/Layout'
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -15,14 +16,16 @@ class App extends Component {
   render() {
     return (
       <>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/signup' component={Signup}/>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/logout' component={Logout}/>
-          </Switch>
-        </Router>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/signup' component={Signup}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/logout' component={Logout}/>
+            </Switch>
+          </Router>
+        </Layout>
       </>
     )
   }

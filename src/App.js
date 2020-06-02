7c7a2d15be12cf7ce440e1d65cf6a803e2fcbd9f
Richard Cho <richardcho100@gmail.com>
2020-06-01 20:39:06 -0400

    Enable routes for signup, login, & logout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Signup from './components/Signup'
 import Login from './components/Login'
 import Logout from './components/Logout'
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
@@ -11,7 +13,15 @@ class App extends Component {
 
   render() {
     return (
-      this.props.currentUser ? <Logout /> : <Login />
+      <>
+        <Router>
+          <Switch>
+            <Route exact path='/signup' component={Signup}/>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/logout' component={Logout}/>
+          </Switch>
+        </Router>
+      </>
     )
   }
 }

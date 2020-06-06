cda3ac01ab131e85f1868033a920a46299494261
Richard Cho <richardcho100@gmail.com>
2020-06-05 23:23:40 -0400

    Add container & pass in top movies as prop from store

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { popularMovies, topRatedMovies } from '../actions/movies'
 import PopularContainer from '../containers/PopularContainer'
+import TopRatedContainer from '../containers/TopRatedContainer'
 
 class Home extends Component {
     componentDidMount() {
@@ -14,6 +15,7 @@ class Home extends Component {
             <div>
                 <h2>Movie Central</h2>
                 <PopularContainer popMovies={this.props.popMovies}/>
+                <TopRatedContainer topMovies={this.props.topMovies}/>
             </div>
         )
     }
@@ -22,6 +24,7 @@ class Home extends Component {
 const mapStateToProps = state => {
     return {
         popMovies: state.movies.popularMovies,
+        topMovies: state.movies.topRatedMovies
     }
 }
 

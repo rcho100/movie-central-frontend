25c5044aef5c4db44ddf1fbbc84e71b1bceae7c3
Richard Cho <richardcho100@gmail.com>
2020-06-05 20:54:28 -0400

    Include case & default key/value for top movies

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,7 +1,9 @@
-export const movies = (state = { popularMovies: [] }, action) => {
+export const movies = (state = { popularMovies: [], topRatedMovies: [] }, action) => {
     switch (action.type) {
         case "RECEIVE_POP_MOVIES":
             return {...state, popularMovies: action.popMovies }
+        case "RECEIVE_TOP_MOVIES":
+            return {...state, topRatedMovies: action.topMovies}
         default:
             return state
     }

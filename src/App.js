1156ce6f0fb0f1f20fe11d3588283dffb674f172
Richard Cho <richardcho100@gmail.com>
2020-06-08 23:15:04 -0400

    Add routes for popular movies & top rated movies

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import NavigationBar from './components/NavigationBar'
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PopularContainer from './containers/PopularContainer';
+import TopRatedContainer from './containers/TopRatedContainer';
 
 class App extends Component {
   componentDidMount() {
@@ -25,6 +27,8 @@ class App extends Component {
               <Route exact path='/signup' component={Signup}/>
               <Route exact path='/login' component={Login}/>
               <Route exact path='/logout' component={Logout}/>
+              <Route exact path='/popular-movies' component={PopularContainer}/>
+              <Route exact path='/top-rated-movies' component={TopRatedContainer}/>
             </Switch>
           </Router>
         </Layout>

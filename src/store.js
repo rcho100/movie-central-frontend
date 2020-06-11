74448a5cce28ad62c2b257b2b761d89403e85324
Richard Cho <richardcho100@gmail.com>
2020-06-11 19:02:26 -0400

    Rename reducer to rootReducer & pass in movieDetails

--- src/store.js
+++ src/store.js
@@ -4,19 +4,21 @@ import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
 import signupForm from './reducers/signupForm'
 import { movies } from './reducers/movies'
+import { movieDetails } from './reducers/movieDetails'
 import thunk from 'redux-thunk';
 
 
-const reducer = combineReducers({
+const rootReducer = combineReducers({
     users,
     currentUser,
     loginForm,
     signupForm,
-    movies
+    movies,
+    movieDetails
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 
 export default store

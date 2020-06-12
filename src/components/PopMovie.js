c1c12b22fe6a1d772ec31b2a8e2392064d3c5832
Richard Cho <richardcho100@gmail.com>
2020-06-12 18:41:44 -0400

    Invoke movieDetails and display details from props

--- src/components/PopMovie.js
+++ src/components/PopMovie.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { movieDetails } from '../actions/movieDetails'
 
+let movieID;
+let movieDisplayed = {};
 
 class PopMovie extends Component {
-    render() {
-        const movieID = this.props.match.params.id
-        const movie = this.props.popMovies.find(movie => parseInt(movie.id, 10) === parseInt(movieID, 10))
+    componentDidMount() {
+        this.props.movieDetails(movieID)
+    }
 
-        return (
-            <div>
-                <h2>{movie.title}</h2>
-                <img alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}/>
-                <p>Release Date: {movie.release_date}</p>
-                <h4>Overview</h4>
-                <p>{movie.overview}</p>
-            </div>
-        )
+    render() {
+            movieID = this.props.match.params.id
+
+            const {id, title, backdrop_path, poster_path, overview, tagline, release_date, runtime} = this.props.movDetails
+            movieDisplayed.id = id
+            movieDisplayed.title = title
+            movieDisplayed.backdrop_path = backdrop_path
+            movieDisplayed.poster_path = poster_path
+            movieDisplayed.overview = overview
+            movieDisplayed.tagline = tagline
+            movieDisplayed.release_date = release_date
+            movieDisplayed.runtime = runtime
+
+            return (
+                <div>
+                    <h2>{movieDisplayed.title}</h2>
+                    <img alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + movieDisplayed.poster_path}/>
+                    <p>Release Date: {movieDisplayed.release_date}</p>
+                    <p>Runtime: {movieDisplayed.runtime} minutes</p>
+                    <h5>{movieDisplayed.tagline}</h5>
+                    <img alt="movie backdrop" src={"https://image.tmdb.org/t/p/w500" + movieDisplayed.backdrop_path}/>
+                    <h4>Overview</h4>
+                    <p>{movieDisplayed.overview}</p>
+                </div>
+            )
     }
 }
 
 const mapStateToProps = state => {
     return {
-        popMovies: state.movies.popularMovies
+        movDetails: state.movieDetails
     }
 }
 
-export default connect(mapStateToProps)(PopMovie)
+export default connect(mapStateToProps, { movieDetails })(PopMovie)

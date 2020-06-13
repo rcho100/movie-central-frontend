eab49f7be856d7fd696a35b92178888fa02e1647
Richard Cho <richardcho100@gmail.com>
2020-06-13 00:41:04 -0400

    Get genres from store & display on page

--- src/components/PopMovie.js
+++ src/components/PopMovie.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { movieDetails } from '../actions/movieDetails'
 
 let movieID;
-let movieDisplayed = {};
 
 class PopMovie extends Component {
     componentDidMount() {
@@ -11,36 +10,36 @@ class PopMovie extends Component {
     }
 
     render() {
-            movieID = this.props.match.params.id
-
-            const {id, title, backdrop_path, poster_path, overview, tagline, release_date, runtime} = this.props.movDetails
-            movieDisplayed.id = id
-            movieDisplayed.title = title
-            movieDisplayed.backdrop_path = backdrop_path
-            movieDisplayed.poster_path = poster_path
-            movieDisplayed.overview = overview
-            movieDisplayed.tagline = tagline
-            movieDisplayed.release_date = release_date
-            movieDisplayed.runtime = runtime
+        movieID = this.props.match.params.id
+        
+        if (this.props.movie) {
+            const {id, title, backdrop_path, poster_path, overview, tagline, release_date, runtime} = this.props.movie
 
             return (
                 <div>
-                    <h2>{movieDisplayed.title}</h2>
-                    <img alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + movieDisplayed.poster_path}/>
-                    <p>Release Date: {movieDisplayed.release_date}</p>
-                    <p>Runtime: {movieDisplayed.runtime} minutes</p>
-                    <h5>{movieDisplayed.tagline}</h5>
-                    <img alt="movie backdrop" src={"https://image.tmdb.org/t/p/w500" + movieDisplayed.backdrop_path}/>
+                    <h2>{title}</h2>
+                    <img alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + poster_path}/>
+                    <p>Release Date: {release_date}</p>
+                    <p>Runtime: {runtime} minutes</p>
+                    <p>Genres: {this.props.genres.join(', ')}</p>
+                    <h5>{tagline}</h5>
+                    <img alt="movie backdrop" src={"https://image.tmdb.org/t/p/w500" + backdrop_path}/>
                     <h4>Overview</h4>
-                    <p>{movieDisplayed.overview}</p>
+                    <p>{overview}</p>
                 </div>
             )
+        } else {
+            return (
+                <div>Loading...</div>
+            )
+        }
     }
 }
 
 const mapStateToProps = state => {
     return {
-        movDetails: state.movieDetails
+        movie: state.movieDetails.details,
+        genres: state.movieDetails.genres
     }
 }
 

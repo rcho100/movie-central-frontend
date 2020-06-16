0727f647b0f2c7c926734d743633a95c009df092
Richard Cho <richardcho100@gmail.com>
2020-06-16 13:39:44 -0400

    Update conditional to reflect new default currentUser

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,7 +8,7 @@ const NavigationBar = ({ currentUser }) => {
             <Navbar.Brand href='/'></Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
-                {Object.keys(currentUser).length === 0 ?
+                {Object.values(currentUser.user).length === 0 ?
                     <Nav className='ml-auto'>
                         <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link href='/login'>Login</Nav.Link></Nav.Item>

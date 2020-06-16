88476cf80afbeb7e234055187e7fcfb4e962079f
Richard Cho <richardcho100@gmail.com>
2020-06-16 10:23:36 -0400

    Change return value of clear current user to empty obj

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -3,7 +3,7 @@ export default (state = {}, action) => {
         case "SET_CURRENT_USER":
             return action.user
         case "CLEAR_CURRENT_USER":
-            return null
+            return {}
         default:
             return state
     }

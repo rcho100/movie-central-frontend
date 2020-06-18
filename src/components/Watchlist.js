1e706a9ed4fd831543621e00eea57d17ddc5af88
Richard Cho <richardcho100@gmail.com>
2020-06-18 19:04:26 -0400

    Display link poster components in watchlist

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Poster from './Poster'
+import { Link } from 'react-router-dom'
 
 const Watchlist = (props) => {
     if (props.currentUser) {
         return (
             <div>
                 <h2>My Watchlist</h2>
+                {props.currentUser.watchlist.map(movie => (
+                    <Link to={`/movies/${movie.attributes.id_from_api}`} key={movie.attributes.title} >
+                        <Poster movie={movie.attributes} />
+                    </Link>
+                ))}
             </div>
         )
     } else {

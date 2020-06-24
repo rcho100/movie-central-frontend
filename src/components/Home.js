0ce442c4bf6b12878002cce984f69b53218bca29
Richard Cho <richardcho100@gmail.com>
2020-06-23 23:58:51 -0400

    Add Jumbotron & welcome message to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h2>Movie Central</h2>
-                <Link to='/popular-movies'>
+                <Jumbotron>
+                    <h1>Movie Central</h1>
+                    <p>Welcome!</p>
+                    <p>Feel free to browse through any of the current popular movies or any of the top rated movies.</p>
+                    <p>Please remember to login to save movies to your Watchlist.</p>
+                </Jumbotron>
+                <Link className='movies-link' to='/popular-movies'>
                     <p>Popular Movies</p>
                 </Link>
-                <Link to='/top-rated-movies'>
+                <Link className='movies-link' to='/top-rated-movies'>
                     <p>Top Rated Movies</p>
                 </Link>
             </div>

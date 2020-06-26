4b9a55c4daf45aa63504835507e66fcafdfba81c
Richard Cho <richardcho100@gmail.com>
2020-06-26 14:13:07 -0400

    Display cards as posters w/some info & change size

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
-const Poster = ({ movie }) => <img alt="movie poster" src={"https://image.tmdb.org/t/p/w500" + movie["poster_path"]}/>
+const Poster = ({ movie }) => {
+    return (
+        <Card>
+            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + movie["poster_path"]} />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>Details</Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
 
 export default Poster

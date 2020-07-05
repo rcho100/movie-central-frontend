acd6ec74e30f7338fe581bdbdadae56ab4367cfd
Richard Cho <richardcho100@gmail.com>
2020-07-05 13:32:55 -0400

    Remove jumbotron & insert container & div

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Container } from 'react-bootstrap'
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <Jumbotron>
+                <Container>
                     <h1>Movie Central</h1>
                     <p>Welcome!</p>
                     <p>Feel free to browse through any of the current popular movies or any of the top rated movies.</p>
                     <p>Please remember to login to save movies to your Watchlist.</p>
-                </Jumbotron>
-                <Link className='movies-link' to='/popular-movies'>
-                    <p>Popular Movies</p>
-                </Link>
-                <Link className='movies-link' to='/top-rated-movies'>
-                    <p>Top Rated Movies</p>
-                </Link>
-            </div>
+                    <div className='main-links'>
+                        <Link className='movies-link' to='/popular-movies'>
+                            <p>Popular Movies</p>
+                        </Link>
+                        <Link className='movies-link' to='/top-rated-movies'>
+                            <p>Top Rated Movies</p>
+                        </Link>
+                    </div>
+                </Container>
         )
     }
 }

e561719c068b9c42b60b46b62833664e64f7c48d
Richard Cho <richardcho100@gmail.com>
2020-07-06 16:01:32 -0400

    Add dropdown option in navbar for movies

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 const NavigationBar = ({ currentUser }) => {
     return (
@@ -10,14 +10,22 @@ const NavigationBar = ({ currentUser }) => {
                 {Object.values(currentUser.user).length === 0 ?
                     <Nav className='ml-auto'>
                         <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
+                        <NavDropdown title="Movies" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
+                            <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
+                        </NavDropdown>
                         <Nav.Item><Nav.Link href='/login'>Login</Nav.Link></Nav.Item>
                         <Nav.Item><Nav.Link href='/signup'>Signup</Nav.Link></Nav.Item>
                     </Nav>
                         :
                     <Nav className='ml-auto'>
                         <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href='/logout'>Logout</Nav.Link></Nav.Item>
+                        <NavDropdown title="Movies" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
+                            <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
+                        </NavDropdown>
                         <Nav.Item><Nav.Link href='/watchlist'>My Watchlist</Nav.Link></Nav.Item>
+                        <Nav.Item><Nav.Link href='/logout'>Logout</Nav.Link></Nav.Item>
                     </Nav>
                 }
             </Navbar.Collapse>

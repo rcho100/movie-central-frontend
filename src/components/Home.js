a6494ebf3a5d885d523252d2dd4705f93b7adad4
Richard Cho <richardcho100@gmail.com>
2020-07-06 16:12:08 -0400

    Change Home component to functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
-class Home extends Component {
-    render() {
-        return (
-                <Container>
-                    <h1>Movie Central</h1>
-                    <p>Welcome!</p>
-                    <p>Feel free to browse through any of the current popular movies or any of the top rated movies.</p>
-                    <p>Please remember to login to save movies to your Watchlist.</p>
-                    <div className='main-links'>
-                        <Link className='movies-link' to='/popular-movies'>
-                            <p>Popular Movies</p>
-                        </Link>
-                        <Link className='movies-link' to='/top-rated-movies'>
-                            <p>Top Rated Movies</p>
-                        </Link>
-                    </div>
-                </Container>
-        )
-    }
+const Home = () => {
+    return (
+        <Container>
+            <h1>Movie Central</h1>
+            <p>Welcome!</p>
+            <p>Feel free to browse through any of the current popular movies or any of the top rated movies.</p>
+            <p>Please remember to login to save movies to your Watchlist.</p>
+            <div className='main-links'>
+                <Link className='movies-link' to='/popular-movies'>
+                    <p>Popular Movies</p>
+                </Link>
+                <Link className='movies-link' to='/top-rated-movies'>
+                    <p>Top Rated Movies</p>
+                </Link>
+            </div>
+        </Container>
+    )
 }
 
 export default Home

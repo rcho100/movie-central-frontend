8be2ea9c32a2e73b2665b884abf5070357b2058f
Richard Cho <richardcho100@gmail.com>
2020-07-07 00:13:44 -0400

    Change to class component with state to control modal

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,34 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import LogoutModal from '../components/LogoutModal'
 
-const NavigationBar = ({ currentUser }) => {
-    return (
-        <Navbar bg="primary" variant="light" expand='lg'>
-            <Navbar.Brand href='/'>Movie Central</Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                {Object.values(currentUser.user).length === 0 ?
-                    <Nav className='ml-auto'>
-                        <NavDropdown title="Movies" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
-                            <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Item><Nav.Link href='/login'>Login</Nav.Link></Nav.Item>
-                        <Nav.Item><Nav.Link href='/signup'>Signup</Nav.Link></Nav.Item>
-                    </Nav>
-                        :
-                    <Nav className='ml-auto'>
-                        <NavDropdown title="Movies" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
-                            <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Item><Nav.Link href='/watchlist'>My Watchlist</Nav.Link></Nav.Item>
-                    </Nav>
-                }
-            </Navbar.Collapse>
-            
-        </Navbar>
-    )
+class NavigationBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            addModalShow: false
+        }
+    }
+    render() {
+        let addModalClose = () => this.setState({
+            addModalShow: false
+        })
+        
+        return (
+            <Navbar bg="primary" variant="light" expand='lg'>
+                <Navbar.Brand href='/'>Movie Central</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    {Object.values(this.props.currentUser.user).length === 0 ?
+                        <Nav className='ml-auto'>
+                            <NavDropdown title="Movies" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
+                                <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Item><Nav.Link href='/login'>Login</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link href='/signup'>Signup</Nav.Link></Nav.Item>
+                        </Nav>
+                            :
+                        <Nav className='ml-auto'>
+                            <NavDropdown title="Movies" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/top-rated-movies">Top Movies</NavDropdown.Item>
+                                <NavDropdown.Item href="/popular-movies">Popular Movies</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Item><Nav.Link href='/watchlist'>My Watchlist</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link href='#' onClick={() => this.setState({ addModalShow: true })}>Logout</Nav.Link></Nav.Item>
+                            <LogoutModal show={this.state.addModalShow} onHide={addModalClose} />
+                        </Nav>
+                    }
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
 export default NavigationBar

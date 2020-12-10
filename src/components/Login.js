3f40e0258232cb0318bc750f85920778fd6409ce
Richard Cho <richardcho100@gmail.com>
2020-12-09 20:14:46 -0500

    Display popcorn img next to login & signup forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import popcorn from '../images/popcorn.jpg'
 
 const Login = ({ loginFormData, updateLoginForm, login, history }) => {
     const handleInputChange = event => {
@@ -24,27 +25,30 @@ const Login = ({ loginFormData, updateLoginForm, login, history }) => {
     }
 
     return (
-       <Container className='form'>
-            <Row>
-                <Col>
-                    <h1>Login</h1>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId='formBasicEmail'>
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' name='email' value={loginFormData.email} onChange={handleInputChange} />
-                        </Form.Group>
+        <div className='form-page'>
+            <img className='form-popcorn' src={popcorn} alt='popcorn'></img>
+            <Container className='form'>
+                    <Row>
+                        <Col>
+                            <h1>Login</h1>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId='formBasicEmail'>
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type='email' placeholder='Enter email' name='email' value={loginFormData.email} onChange={handleInputChange} />
+                                </Form.Group>
 
-                        <Form.Group controlId='formBasicPassword'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Password' name='password' value={loginFormData.password} onChange={handleInputChange} />
-                        </Form.Group>
-                        <Button variant='primary' type='submit'>
-                        Login
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-       </Container>
+                                <Form.Group controlId='formBasicPassword'>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type='password' placeholder='Password' name='password' value={loginFormData.password} onChange={handleInputChange} />
+                                </Form.Group>
+                                <Button variant='primary' type='submit'>
+                                Login
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+            </Container>
+       </div>
     )
 }
 
